66b16b873eddd0a383d173d2b5b0d6682a890ca3
Planebagels <64249106+Planebagels@users.noreply.github.com>
2020-11-09 09:15:03 -0500

    cmd script Alpha 1.1.2
    Whats new in 1.1.2 alpha:
    - replaced else if with elseif, so I won't need to use so many ends
    - got rid of if's alltogether by using a table 
    - Using a Main loop to run commands

--- main.lua
+++ main.lua
@@ -1,61 +1,46 @@
-print "cmd script beta 0.1"
 
+print "cmd script alpha 1.2"
 
+local function wait (cmdSleepGetNum) -- you can name this function
+-- whatever you want, it doesn't matter.
+  local start = os.time ()
+  repeat until os.time () > start + cmdSleepGetNum
+end
+
+local commands = {
+    help = function ()
+        print "you'll find it out"
+    end,
+    say = function (arg)
+        if arg == '' then
+            print 'what do you want to say?'
+            arg = io.read ()
+        end
+        print (arg)
+    end,
+    stop = function ()
+        os.exit ()
+    end,
+    sleep = function (arg)
+        if arg == '' then
+            print 'how long?'
+            arg = tonumber (io.read ())
+        end
+        wait (tonumber (arg))
+    end,
+    rand = function ()      
+        local randNum = math.random ()
+        print (randNum)
+    end,
+    [false] = function ()   -- fallback.
+        print 'Unknown command'
+    end
+}
+
+-- Main loop:
 while true do
-
-function wait(cmdSleepGetNum)-- you can name this function
---what ever you want, it doesnt matter
-  local start = os.time()
-  repeat until os.time() > start + cmdSleepGetNum
-end
-
-local input = io.read()
-
-
-if input == "/help" then
-print"you'll find it out"
-
-
-else if input == "/say" then
-print"what do you want to say?"
-local cmdSay = io.read()
-print(cmdSay)
-
-
-
-else if input == "/stop" then
-os.exit()
-
-
-
-else if input == "/sleep" then
-print"how long?"
-local cmdSleepGetNum = io.read()
-wait(cmdSleepGetNum)
-
-
-
-else if input == "/rand" then
-local randNum = math.random()
-print(randNum)
-
-
-
-else if input == "/extra" then
-print"i am cooler than you"
-
-
-end
-end
-end
-end
-end
-end
-
-
-
-
-
-
-
+    io.write '> '
+    local key, _, arg = io.read ():match '^%s*/(%S+)(%s*(.*))$' -- you can type /sleep 1, etc. in one line.
+    local command = key and key ~= '' and commands [key] or commands [false]
+    command (arg)
 end
